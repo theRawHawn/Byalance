30199b4a8874788044e4f563bf1c0016e5001e61
rohankulkarni.rk6 <rohankulkarni.rk6@gmail.com>
2025-10-13 11:17:14 +0000

    Fix build errors

--- components/financial-toolkit.tsx
+++ components/financial-toolkit.tsx
@@ -216,7 +216,7 @@ export default function FinancialToolkit() {
                       <Input placeholder="Enter your email" type="email" {...field} />
                     </FormControl>
                     <FormMessage />
-                  </Ttem>
+                  </FormItem>
                 )}
               />
 
