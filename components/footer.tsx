21a7103e268d01162c63482862d7ba847078704a
Rohan Kulakarni <70747964+theRawHawn@users.noreply.github.com>
2025-10-29 15:34:27 +0530

    Updated logo alignment in footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
 
   return (
     <footer className="bg-gray-900 text-white py-12">
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-7">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="flex-shrink-0"> <br />
             <Image src="/brandlogowhite.png"
