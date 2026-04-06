61ca9b7e7f1badbf57d064849843fe5687e680d4
Rohan Kulakarni <70747964+theRawHawn@users.noreply.github.com>
2026-04-06 15:11:51 +0530

    Updated Whatsapp Number in footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -162,7 +162,7 @@ export default function Footer() {
                 <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
                 </svg>
-                +91 9380597443
+                +91 7406296116
               </li>
               <li className="flex items-center">
                 <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
